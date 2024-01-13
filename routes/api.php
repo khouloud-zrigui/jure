835793aa621e2cee0a6833f251b7aa0d34097598
Amira Dridi <134102244+Amiradri@users.noreply.github.com>
2024-01-13 17:41:34 +0100

    create links crud (#3)

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+use App\Http\Controllers\Api\LinkController;
+Route::get('link',[LinkController::class,'get']);
+Route::post('link',[LinkController::class,'add']);
+Route::put('link/edit/{id}',[LinkController::class,'update']);
+Route::delete('link/delete/{id}',[LinkController::class,'delete']);
+Route::get('link/get/{id}',[LinkController::class,'getById']);
+Route::get('link/by-href/{href}',[LinkController::class,'getByHref']);
