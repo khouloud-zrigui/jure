55d62918b101220a722a8e6d5863095d6e6b7ff3
pc <askriamani01@gmail.com>
2024-01-13 11:16:45 +0100

    fix-create table&controllers

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TweetController;
+use App\Http\Controllers\SponsorsController;
+use App\Http\Controllers\OrganizersController;
+use App\Http\Controllers\KeynotespeakersController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,6 +20,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', function () {
+    return ('welcome');
+});
+//TWEETS
+Route::post('/tweets', [TweetController::class, 'store']);
+Route::get('/tweets', [TweetController::class, 'index']);
+Route::get('/tweets/{id}', [TweetController::class, 'show']);
+Route::delete('/tweets/{id}', [TweetController::class, 'destroy']);
+
+//sponsors
+Route::get('/sponsors', [SponsorsController::class, 'index']);
+Route::get('/sponsors/{id}', [SponsorsController::class, 'show']);
+Route::post('/sponsors', [SponsorsController::class, 'store']);
+
+//Organizers
+Route::get('/organizers', [OrganizersController::class, 'index']);
+Route::get('/organizers/{id}', [OrganizersController::class, 'show']);
+//KEYNOTESPEAKERS
+Route::get('/keynotespeakers', [KeynotespeakersController::class, 'index']);
+Route::get('/keynotespeakers/{id}', [KeynotespeakersController::class, 'show']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
